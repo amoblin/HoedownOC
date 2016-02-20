e8b850f208c1233f81f6018ac6fd972e54cedf34
amoblin@mbp <amoblin@gmail.com>
2016-02-20 11:52:59 +0800

    Initial Commit

---
+++ HoedownOCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HoedownOCDemo
+//
+//  Created by amoblin on 16/2/20.
+//  Copyright © 2016年 marboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HoedownOCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HoedownOCDemo
+//
+//  Created by amoblin on 16/2/20.
+//  Copyright © 2016年 marboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
